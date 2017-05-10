2ed915a8d93a3cf37a556cf444d8d11426a93a3b
flesk <eivindkn@gmail.com>
2017-05-10 23:26:44 +0200

    Add "dialogue anchor" for off-screen talking character

---
+++ globals/dialog_anchor.gd
@@ -0,0 +1,14 @@
+extends "res://globals/interactive.gd"
+
+export var global_id = ""
+
+func set_state(p_state, p_force = false):
+	pass
+
+func set_speaking(p_speaking):
+	pass
+	
+func _ready():
+	if global_id != "":
+		vm = get_tree().get_root().get_node("vm")
+		vm.register_object(global_id, self)
