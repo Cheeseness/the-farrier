74baa9a6399239e02c8d3ccd96408707be5898cd
flesk <eivindkn@gmail.com>
2017-05-19 00:48:31 +0200

    Don't add customer to global

--- scenes/rooms/reception_area/dialogue_loop.gd
+++ scenes/rooms/reception_area/dialogue_loop.gd
@@ -8,12 +8,10 @@ func global_listener(name):
 
 # Function is first started by timer to dodge race conditions
 func start_dialogue():
-	var customer = vm.get_global("customer")
+	var customer = get_parent().get_node("PlaceholderCustomer")
 	get_tree().call_group(0, "game", "clicked", customer, customer.get_pos())
 
 func _ready():
-	printt("customer scene is ready")
 	vm = get_tree().get_root().get_node("/root/vm")
-	vm.set_global("customer", get_parent().get_node("PlaceholderCustomer"))
 	# Use listener to restart dialogue tree
 	vm.connect("global_changed", self, "global_listener")
