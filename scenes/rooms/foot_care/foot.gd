6e98bbeb22cf1ee166c8015e8d0bb6332d46785e
flesk <eivindkn@gmail.com>
2017-06-13 22:20:14 +0200

    Instance splinters at positions

--- scenes/rooms/foot_care/foot.gd
+++ scenes/rooms/foot_care/foot.gd
@@ -6,6 +6,7 @@ var comfort = {
 	"indicator": null
 }
 var splinters_removed = 0
+const splinter_scene = preload("res://scenes/conditions/splinter/splinter.tscn")
 
 func set_comfort_level(increase=true):
 	if increase:
@@ -36,7 +37,14 @@ func input(viewport, event, shape_idx):
 							vm.set_global("splinter_removed", true)
 							if splinters_removed == 3:
 								vm.set_global("splinters_removed", true)
-					
+
+func add_splinters():
+	for child in get_children():
+		if child.get_type() == "Position2D":
+			var splinter = splinter_scene.instance()
+			splinter.set_pos(child.get_pos())
+			splinter.set_scale(Vector2(0.648465, 0.648465))
+			add_child(splinter)
 
 func _ready():
 	vm = get_tree().get_root().get_node("/root/vm")
@@ -45,6 +53,8 @@ func _ready():
 	# TODO Set initial dino sounds somewhere (else?)
 	vm.set_global("dino_hello", "*ergjbld*")
 	vm.set_global("dino_goodbye", "*asdfkld*")
+
+	add_splinters()
 
 	# Hook up a temporary comfort level indicator
 	# TODO Change facial expression instead
